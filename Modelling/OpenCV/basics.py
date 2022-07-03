c625e9df38f1ece2c37d1ad93b99611ba8cd6fb5
DuKe786 <71754779+mohitmishra786@users.noreply.github.com>
2022-07-04 00:50:19 +0530

    Add

--- Modelling/OpenCV/basics.py
+++ Modelling/OpenCV/basics.py
@@ -1,7 +1,8 @@
+import cv2
 import numpy as np
 import cv2 as cv
 
-img = cv.imread(r'C:\Users\mohit\OneDrive\Desktop\OpenCV\images\download.jfif')
+img = cv.imread(r'C:\Users\mohit\OneDrive\Desktop\My-Machine-Learning-And-Deep-Learning-Work\Modelling\OpenCV\images\download.jfif')
 cv.imshow('Anime' , img)
 
 
@@ -21,7 +22,21 @@ cv.imshow('Canny' , canny)
 canny = cv.Canny(blur , 125 , 175)
 cv.imshow('Canny' , canny)
 
-## Dilating Images
+## Dilating Images, it will make the edges more dark and bold
+dilated = cv.dilate(canny , (7,7) , iterations=3)
+cv.imshow('Dilated' , dilated)
 
-dilated = cv.
+
+## Eroding the images, simply means turning back dilated image into canny, but it will not be exact same
+eroded = cv.erode(dilated , (7,7) , iterations=3)
+cv.imshow('Erodede' , eroded)
+
+## Resizing
+resized = cv.resize(img , (500 , 500) , interpolation= cv.INTER_CUBIC)
+cv.imshow("Resized" , resized)
+
+## Cropping
+cropped = img[50:200 , 200:300]
+cv.imshow('Cropped' , cropped)
+
 cv.waitKey(0)
